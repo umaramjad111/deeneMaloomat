12aaf7b579b924a1768b09e3c9d393496cf2d4e8
umaramjad <umaramjad489@gmail.com>
2023-08-29 15:45:51 +0500

    set

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import './App.css';
-// import Bayan from './components/bayan/bayan';
-// import Navbar from './components/navbar/navbar';
-// import Quran from './components/quran/quran';
-// import { Makkah } from './utils/image';
-
 function App() {
-
-  
-  
-  return (
-    <div className="App">
-     {/* <Navbar/>
-      <img src={Makkah} alt="" className='w-100' />
-      <Bayan/>
-      <Quran/> */}
-      <h5>fsdf</h5>
-    </div>
-  );
+  return <h5>fsdf</h5>;
 }
 
 export default App;
